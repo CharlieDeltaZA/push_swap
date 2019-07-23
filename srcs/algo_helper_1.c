40fa152e71e38dea638acc296a2a2ec21b5b6a24
Calvin Diogo <calvin@diogo.co.za>
2019-07-23 09:15:56 +0200

    Update algo_helper_1.c

--- srcs/algo_helper_1.c
+++ srcs/algo_helper_1.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/23 08:36:50 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/07/23 08:50:54 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/07/23 09:15:44 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,46 @@ int		find_largest_pos(t_stack **head, int largest)
 	while (node)
 	{
 		if (node->value == largest)
+		{
+			pos = i;
+			break ;
+		}
+		node = node->next;
+		i++;
+	}
+	return (pos);
+}
+
+int		find_smallest(t_stack **head)
+{
+	//TODO --Erm. 0 might break it
+	t_stack	*node;
+	int		smallest;
+
+	node = *head;
+	smallest = 0;
+	while (node)
+	{
+		if (node->value < smallest)
+			smallest = node->value;
+		node = node->next;
+	}
+	return (smallest);
+}
+
+int		find_smallest_pos(t_stack **head, int smallest)
+{
+	//TODO
+	t_stack	*node;
+	int 	pos;
+	int		i;
+
+	node = *head;
+	pos = 0;
+	i = 0;
+	while (node)
+	{
+		if (node->value == smallest)
 		{
 			pos = i;
 			break ;
