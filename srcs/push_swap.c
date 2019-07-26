2216f2d3ede20c98b1fd18ad22c5ccc00d147c7e
Calvin Diogo <calvin@diogo.co.za>
2019-07-26 19:29:36 +0200

    Update push_swap.c

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/01 15:04:09 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/07/26 14:32:49 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/07/26 19:27:56 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int		main(int ac, char **av)
 	// size_a = stack_size(a);
 //	DEBUG_2;
 	normalize(&a);
-//	DEBUG_2;
+	DEBUG_2;
 	///////////////////////// Sort4 + 5 - find the smallest vals, push b, sort3, pa
 	sort_master(&a, &b);
 	// if (size_a == 2)
