d894a608b84905e67d8f060fdf80a241f9ff0741
Calvin Diogo <calvin@diogo.co.za>
2019-07-27 12:39:14 +0200

    Update algo_helper_2.c

--- srcs/algo_helper_2.c
+++ srcs/algo_helper_2.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/26 13:50:05 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/07/26 19:54:36 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/07/27 12:31:05 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 /*
 ** Determines how many times to ra or rra to get a number to top of stack.
+** Used in sort_5
 */
 
 //MIGHT BREAK - Even stack sizes / or after stack size decreases by one in a call
@@ -38,6 +39,34 @@ void	ra_rra_x_times(t_stack **head, int pos)
 		while (pos != 0)
 		{
 			ra(head, 1);
+			pos--;
+		}
+	}
+}
+
+/*
+** Determines how many times to rb or rrb to get a number to top of stack.
+*/
+
+void	rb_rrb_x_times(t_stack **head, int pos)
+{
+	int		stacksize;
+
+	stacksize = stack_size(*head);
+	if (pos > stacksize / 2)
+	{
+		pos = stacksize - pos;
+		while (pos != 0)
+		{
+			rrb(head, 1);
+			pos--;
+		}
+	}
+	else if (pos <= stacksize / 2)
+	{
+		while (pos != 0)
+		{
+			rb(head, 1);
 			pos--;
 		}
 	}
