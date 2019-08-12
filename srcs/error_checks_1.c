d95e92a2b0f158bfee7dc5de985e6f9ad6c1853a
Calvin Diogo <calvin@diogo.co.za>
2019-08-12 09:13:50 +0200

    English error

--- srcs/error_checks_1.c
+++ srcs/error_checks_1.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 14:44:23 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/08/08 13:32:21 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/08/12 09:13:34 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 /*
 ** Determine the error message to output based on code passed
-** 1 - Invalid Inputs ; 2 - Input over INT_MAX ; 3 - Input over INT_MIN ;
+** 1 - Invalid Inputs ; 2 - Input Over INT_MAX ; 3 - Input Under INT_MIN ;
 ** 4 - Duplicate Inputs ; 5 - Incorrect Operation ; 6 - Incorrect Arg ;
 ** STDERR = fd of 2
 */
@@ -26,7 +26,7 @@ void	error_out(int code)
 	if (code == MAXSIZE)
 		ft_putendl_col_fd(RED, "Error: Input Over INT_MAX", 2);
 	if (code == MINSIZE)
-		ft_putendl_col_fd(RED, "Error: Input Over INT_MIN", 2);
+		ft_putendl_col_fd(RED, "Error: Input Under INT_MIN", 2);
 	if (code == DUPE)
 		ft_putendl_col_fd(RED, "Error: Duplicate Inputs", 2);
 	if (code == OPS)
